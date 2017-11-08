33fc64ebf7972239b148078a53db568d81d57104
Nikolay Ivanov <nivanov@detmir.ru>
2017-11-08 13:56:08 +0300

    Change gradle version

--- web/src/main/java/org/lighthouse/LighthouseWebApplication.java
+++ web/src/main/java/org/lighthouse/LighthouseWebApplication.java
@@ -1,6 +1,8 @@
 package org.lighthouse;
 
+import org.lighthouse.domain.entities.Country;
 import org.lighthouse.domain.entities.SystemUser;
+import org.lighthouse.domain.repositories.CountryRepository;
 import org.lighthouse.domain.repositories.UserRepository;
 import org.lighthouse.domain.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,11 +20,13 @@ import org.springframework.context.annotation.Bean;
 public class LighthouseWebApplication {
     private final UserService userService;
     private final UserRepository userRepository;
+    private final CountryRepository countryRepository;
 
     @Autowired
-    public LighthouseWebApplication(UserRepository userRepository, UserService userService) {
+    public LighthouseWebApplication(UserRepository userRepository, UserService userService, CountryRepository countryRepository) {
         this.userRepository = userRepository;
         this.userService = userService;
+        this.countryRepository = countryRepository;
     }
 
     public static void main(String[] args) {
@@ -37,6 +41,13 @@ public class LighthouseWebApplication {
             systemUser.setPassword("admin");
             if (!userRepository.findByUsername("admin").isPresent()) {
                 userService.createUser(systemUser);
+            }
+
+            if (!countryRepository.findByName("Болгария").isPresent()) {
+                Country country = new Country();
+                country.setName("Болгария");
+                country.setSiteUrl("http://register.bulnao.government.bg");
+                countryRepository.save(country);
             }
         };
     }
